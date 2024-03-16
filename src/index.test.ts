eb35a480d98304263548d5cb41c16df137592567
Elias <eeschmoelz@gmail.com>
2024-03-16 22:06:59 +0000

    test descriptions

--- src/index.test.ts
+++ src/index.test.ts
@@ -2,14 +2,14 @@ import { expect, describe, it } from 'vitest'
 import app from '.'
 
 describe('Requests', () => {
-  describe('Requests - Fresh', () => {
-    it('should return 500 due to missing clerk env variables', async () => {
+  describe('Requests - Unauthenticated', () => {
+    it('should return 401 as an unauthenticated user', async () => {
       const res = await app.request("/v1/test", { method: 'GET' });
       expect(res.status).toBe(401);
     })
   })
 
-  describe('Requests - Warm', () => {
+  describe('Requests - Public', () => {
     it('should return pong', async () => {
       const res = await app.request("/ping", { method: 'GET' });
       expect(res.status).toBe(200);
