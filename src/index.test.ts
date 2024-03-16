3c519b8afc90aeeea6c6f239a997d066ba582215
Elias <eeschmoelz@gmail.com>
2024-03-16 20:37:39 +0000

    tests for entry file

---
+++ src/index.test.ts
@@ -0,0 +1,21 @@
+import { expect, describe, it, beforeAll } from 'vitest'
+import app from '.'
+
+describe('Requests', () => {
+  describe('Requests - Fresh', () => {
+    it('should return 500 due to missing clerk env variables', async () => {
+      const res = await app.request("/v1/test", { method: 'GET' });
+      expect(res.status).toBe(500);
+    })
+  })
+
+  describe('Requests - Warm', () => {
+    it('should return pong', async () => {
+      const res = await app.request("/ping", { method: 'GET' });
+      expect(res.status).toBe(200);
+      expect(await res.json()).toEqual({
+        ping: "pong"
+      })
+    })
+  })
+})
